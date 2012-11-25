702a8c4d614dca1a1c1d6ccb1dfdf4d798b2717d
PKNzeta <pkno3o327676>
2012-11-25 21:46:03 +0100

    unsignifient commit
    A commit, just about testing git functionnality :-\

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 
 #include <stdlib.h>
-#include <stdio.h>
 #include "gfx.h"
 #include "toolkit_monster_edit.h"
 
 int main (int argc, char** argv)
 {
+    /* avoid compiler warnings */
+    (void) argv; (void) argc;
+
     atexit (gfx_free);
     gfx_init ();
     monster_editor ();
