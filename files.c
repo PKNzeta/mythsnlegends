9b6f5a9569fd8e7e68a978cb6c70bd6b0accb317
PKNzeta <pkno3o327676>
2012-11-30 00:04:37 +0100

    files.c improvement and documentation

--- files.c
+++ files.c
@@ -1,33 +1,13 @@
+/** files.c
+ * TODO: - files_closeall ()
+ *         implement a opened file list so we can free them all
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-#ifdef WIN32
-
-#define N_BASEDIR 1
-#define PTH_SEP "\\"
-
-static const char* base_dir[N_BASEDIR] =
-{
-    ".\\"
-};
-
-#else
-
-#define N_BASEDIR 4
-#define PTH_SEP "/"
-static const char* base_dir[N_BASEDIR] =
-{
-    "./",
-    "/usr/share/" PROGRAM_NAME "/",
-    "/usr/local/share/" PROGRAM_NAME "/",
-    "~/.share/" PROGRAM_NAME "/"
-};
-
-#endif
-
-static char locate_buf[128] = {'\0'};
-
+/* Private definitions ********************************************************/
 
 typedef struct spath_struct
 {
@@ -35,10 +15,47 @@ typedef struct spath_struct
     struct spath_struct* nxt;
 } SPATH;
 
+#ifdef WIN32
+#  define N_BASEDIR 1
+#  define PTH_SEP "\\"
+#else
+#  define N_BASEDIR 4
+#  define PTH_SEP "/"
+#endif
 
-SPATH* paths = NULL;
+/* Private data ***************************************************************/
+
+#ifdef WIN32
+static const char* base_dir[N_BASEDIR] =
+{
+    ".\\"
+};
+#else
+static const char* base_dir[N_BASEDIR] =
+{
+    "./",
+    "/usr/share/" PROGRAM_NAME "/",
+    "/usr/local/share/" PROGRAM_NAME "/",
+    "~/.share/" PROGRAM_NAME "/"
+};
+#endif
 
 
+/* A buffer to where format paths */
+static char pth_buf[128] = {'\0'};
+
+/* a chained list of all the search paths */
+static SPATH* paths = NULL;
+
+/* Public functions ***********************************************************/
+
+/* files_add_search_path ()
+ * Add a the search path "pth". the path is relative to installation directory.
+ * for exemple let's say that your program has a directory named data. the full
+ * path will be; "/usr/share/program_name/data. then you must call this function
+ * like this: files_add_search_path("data");
+ * now, files will be searched in this directory
+ */
 void files_add_search_path (const char* pth)
 {
     SPATH* newpth = malloc (sizeof (SPATH));
@@ -47,19 +64,25 @@ void files_add_search_path (const char* pth)
     paths = newpth;
 }
 
+
+/* files_open ()
+ * try to find and open the given file "filename" into the different given
+ * search paths
+ * filename: the file name without path.
+ * return: a FILE* pointing to the opened file or NULL on fail.
+ */
 FILE* files_open (const char* filename)
 {
     FILE*  file = NULL;
+    SPATH* cur = NULL;
     int i = 0;
 
     while (i < N_BASEDIR && file == NULL)
     {
-        SPATH* cur = paths;
+        cur = paths;
 
         while (cur != NULL && file == NULL)
         {
-            char pth_buf[128] = {'\0'};
-
             snprintf (pth_buf, 127, "%s%s" PTH_SEP "%s",
                       base_dir[i], cur->str, filename);
             file = fopen (pth_buf, "r");
@@ -71,32 +94,22 @@ FILE* files_open (const char* filename)
     return file;
 }
 
+
+/* files_locate ()
+ * Try to find if the given file "filename" is present in the search paths.
+ * filename: the filename we are looking for.
+ * return: a constant string of the full path of filename or NULL if none found.
+ *         take care of copying the result and not just point to it as it's
+ *         subject to changes.
+ */
 const char* files_locate (const char* filename)
 {
-    FILE*  file = NULL;
-    int i = 0;
+    FILE*  file = files_open (filename);
 
-    while (i < N_BASEDIR && file == NULL)
+    if (file != NULL)
     {
-        SPATH* cur = paths;
-
-        while (cur != NULL && file == NULL)
-        {
-            char pth_buf[128] = {'\0'};
-
-            snprintf (pth_buf, 127, "%s%s" PTH_SEP "%s",
-                      base_dir[i], cur->str, filename);
-            if ((file = fopen (pth_buf, "r")) != NULL)
-            {
-                strncpy (locate_buf, (const char*) pth_buf, 127);
-                fclose (file);
-                break;
-            }
-
-            cur = cur->nxt;
-        }
-        i++;
+        fclose (file);
     }
 
-    return (file)? locate_buf : NULL;
+    return (file != NULL)? pth_buf : NULL;
 }
--- files.h
+++ files.h
@@ -1,10 +1,32 @@
 #ifndef H__FILES
 #define H__FILES
 
+/* files_add_search_path ()
+ * Add a the search path "pth". the path is relative to installation directory.
+ * for exemple let's say that your program has a directory named data. the full
+ * path will be; "/usr/share/program_name/data. then you must call this function
+ * like this: files_add_search_path("data");
+ * now, files will be searched in this directory
+ */
 void files_add_search_path (const char* pth);
 
+
+/* files_open ()
+ * try to find and open the given file "filename" into the different given
+ * search paths
+ * filename: the file name without path.
+ * return: a FILE* pointing to the opened file or NULL on fail.
+ */
 FILE* files_open (const char* filename);
 
+
+/* files_locate ()
+ * Try to find if the given file "filename" is present in the search paths.
+ * filename: the filename we are looking for.
+ * return: a constant string of the full path of filename or NULL if none found.
+ *         take care of copying the result and not just point to it as it's
+ *         subject to changes.
+ */
 const char* files_locate (const char* filename);
 
 #endif
